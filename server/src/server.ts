de359b36b8dfee794dccc1aa73e12ff70329661b
Karolina Lubczańska <lupa319@gmail.com>
2025-08-25 21:25:55 +0200

    changed url

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,17 +2,14 @@ import app from "./app";
 import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
+const port = env.PORT || 5000;
 
-
-const port = env.PORT;
-
-mongoose.connect(env.MONGO_CONNECTION_URL)
-.then(() => {
+mongoose
+  .connect(env.MONGO_CONNECTION_URL)
+  .then(() => {
     console.log("Mongoose connected");
     app.listen(port, () => {
-        console.log("server running on port: " + port);
+      console.log("server running on port: " + port);
     });
-
-})
-.catch(console.error);
-
+  })
+  .catch(console.error);
